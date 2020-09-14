766962c58a1efd2155b7752ad2577b89cff7f4de
Muhammad Ali <kustalics@gmail.com>
2020-09-14 22:48:55 +0500

    first commit

---
+++ Acore/Acore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Acore.MvcApplication" Language="C#" %>
---
+++ Acore/Acore/Models/Payment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Acore.Models
+{
+    public class Payment
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+      
+    }
+    public class PaymentDBContext : DbContext
+    {
+        public DbSet<Payment> Payments { get; set; }
+    }
+
+}
